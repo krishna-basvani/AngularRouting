f15893467dea7bea9638267a146310942d1bfd26
bhagavan.basvani@capgemini.com <bhagavan.basvani@capgemini.com>
2020-04-04 21:35:35 +0530

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'welcome',component:AppComponent},
+      {path:'',redirectTo:'welcome', pathMatch:'Full'},
+      {path:'**',component:ErrorComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
